9b9d0cbb3e9bac075d56f3dbd1946004af6353ad
Maalik McBurrows <mmcburrows1@gmail.com>
2020-04-03 16:21:13 -0400

    debugged some code with sean, still some issues with the route/model, updated sql schema and seed with current database values

--- queries.sql
+++ queries.sql
@@ -1,22 +1,28 @@
+-- I left many of the foreign key constraints blank for the fact that while constructing the database some of the primary keys 
+-- have not been generated yet, I'd reccomend just going back after you create the database and updating those fields with their official corresponding values.
+
 CREATE DATABASE coronablog;
 
 CREATE TABLE users (id serial PRIMARY KEY, first_name text, last_name text, entryid int REFERENCES blog(id));
 
-INSERT INTO users (first_name, last_name, entryid) VALUES ('Maalik', 'McBurrows', 1);
+INSERT INTO users (first_name, last_name, entryid) VALUES ('Maalik', 'McBurrows', null);
+INSERT INTO users (first_name, last_name, entryid) VALUES ('Coronita', 'Virusia', null);
 
 
 CREATE TABLE blog (id serial PRIMARY KEY,
                                         name text NOT NULL,
                                                   title varchar, entry varchar, userid int REFERENCES users(id));
-
 INSERT INTO blog (name, title, entry, userid) 
-VALUES ('Maalik', '04-02', 'Super boring, but at least I have code, twitter, and PS4 lol', 1);
+VALUES ('Maalik', '04-02', 'Super boring, but at least I have code, twitter, and PS4 lol', null);
+INSERT INTO blog (name, title, entry, userid) 
+VALUES ('Coronita Virusia', '04-03', 'I have been able to travel the world for free, I am more cultured because of this great experience :)', null);
+
 
 CREATE TABLE comments (id serial PRIMARY KEY,
                                         first_name text NOT NULL,
-                                                  last_name text, text varchar, blogid int REFERENCES blog(id));
-                                            
-INSERT INTO comments (first_name, last_name, text, blogid) VALUES ('Ya', 'Boi', 'Great Comment', 1);
+                                                  last_name text, text varchar, blogid int REFERENCES blog(id));                                      
+INSERT INTO comments (first_name, last_name, text, blogid) VALUES ('Ya', 'Boi', 'Great Comment', null);
+INSERT INTO comments (first_name, last_name, text, blogid) VALUES ('Coronita', 'Virusia', '@ me next time bro', null);
 
 
 
--- routes/blogs.js
+++ routes/blogs.js
@@ -2,25 +2,25 @@ var express = require('express');
 var router = express.Router();
 const blogModel = require('../models/blogModel');
 
-router.get('/blogs', async (res) => {
+router.get('/', async (req, res) => {
     const response = await blogModel.getAllBlogData();
-    res.json(response);
+    res.json(response).status(200);
 });
 
-router.get('/blogs/:id?', async (req, res) => {
+router.get('/:id?', async (req, res) => {
     const {
         id
     } = req.params;
     const response = await blogModel.getSingleBlog(id);
-    res.json(response);
+    res.json(response).status(200);
 })
 
-router.get('/blogs/:id?/comments', async (req, res) => {
+router.get('/:id?/comments', async (req, res) => {
     const {
         id
     } = req.params;
     const response = await blogModel.getSingleComment(id);
-    res.json(response);
+    res.json(response).status(200);
 })
 
 module.exports = router;
